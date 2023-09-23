2687bd56d54e09a053a0367fc4c3e3d400da995e
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-09-23 23:18:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 import fs from "fs/promises";
-import {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-} from "./db/contacts.js";
+// import {listContacts, getContactById, removeContact, addContact} from "./db/contacts"
 
-console.log(listContacts);
-// const func = async () => {
-//   // #1
-//   const filepath = "./db/contacts.json";
-//   //   const buffer = await fs.readFile(filepath);
-//   //   console.log(buffer);
-//   //   const text = buffer.toString();
-//   //   console.log(text);
-//   // #2
-//   const text = await fs.readFile(filepath, "utf-8");
-//   console.log(text);
-// };
+const func = async () => {
+  // #1
+  const filepath = "./db/contacts.json";
+  //   const buffer = await fs.readFile(filepath);
+  //   console.log(buffer);
+  //   const text = buffer.toString();
+  //   console.log(text);
+  // #2
+  const text = await fs.readFile(filepath, "utf-8");
+  console.log(text);
+};
 
-// func();
+func();
